bf017941c18b5eb0251f8430ea969584f95931f9
Kim-Young-Jun <kimyj359@gmail.com>
2020-08-05 15:58:23 +0900

    main script update

--- script/hyperparam.py
+++ script/hyperparam.py
@@ -7,21 +7,24 @@ import numpy as np
 class Hyperparam():
 
     ## directory ##
+    make_data = True
+    train = "infer" # "transformer" / "non_cnn" / "infer"
+    infer = "seq2seq"
 
     # raw data
-    textpath = "../data/emo_data.txt"
+    textpath = "../data/emotion_train.txt"
 
     # processed vocab & train data
     datapath = "../data/processed_emo"
 
-    # log dir for cnn & transformer
-    logdir_c = '../logdir_c_new'  # cnn
-    logdir_t = '../logdir_t_n' # transformer
-    logdir_s = '../logdir_s_n'
+    # log dir for cnn & transformer (do not use numbers for the name)
+    logdir_c = '../logdir_c'  # cnn
+    logdir_t = '../logdir_t' # transformer
+    logdir_s = '../logdir_s_new'
 
     # for inference
-    fout = "../song_out.txt"        # out file
-    ftest = "../data/test_talk_2.txt" # test text
+    fout = "../out.txt"        # out file
+    ftest = "../data/test_talk.txt" # test text
 
     morfessor = False
     postprocess = False
@@ -36,29 +39,30 @@ class Hyperparam():
 
     ## training ##
 
-    batch_size = 32
+    batch_size = 8
     lr = 1e-3     # leanring rate
     emb_size_cnn = 256
     emb_size_sent = 248
     emb_size_tag = 8
 
+    # for cnn
     filter_sizes = [3, 4, 5]
     num_filters = 256
 
-    #hidden_units = emb_size_sent + emb_size_tag
+    ## hidden_units = emb_size_sent + emb_size_tag
+    ## for transformer
     hidden_units = 256
     num_blocks = 6  # number of encoder/decoder blocks (multi-attention)
     num_heads = 8
     dropout_prob = 0.2
     sinusoid = False
 
+    # for sequence-to-sequence
     rnn_size = 256
     num_layers = 4
     clip = 5
-
     beam_width = 3
 
     # epochs
-    num_epochs_total = 10
     num_epochs_cnn = 5
-    num_epochs_trans = 10
+    num_epochs_main = 20
--- script/main.py
+++ script/main.py
@@ -6,39 +6,51 @@ from text_cnn.test_cnn import test
 from misc.data_prep import *
 from hyperparam import Hyperparam as hp
 from transformer.train_transformer import train_transformer
-from transformer.eval import eval
+from transformer.eval_tf import eval_tf
 from seq2seq.train_model import train_seq2seq
 from seq2seq.eval_model import eval_seq2seq
 from non_cnn.train_model_v2 import train_seq2seq_v2
 from non_cnn.eval_model_v2 import eval_seq2seq_v2
 
-make_Data = False
-train = True
-inference = False
 
 # create necessary data and save
-if make_Data:
+if hp.make_data:
     if os.path.exists(hp.datapath):
         import shutil
         shutil.rmtree(hp.datapath)
     create_data()
 
 # train CNN and Transformer network
-if train:
+if hp.train != "infer":
+
+    # if data file is not made correctly, delete and renew data files
     if not os.path.exists(os.path.join(hp.datapath, "rev_dict.pkl")):
         if os.path.exists(hp.datapath):
             import shutil
             shutil.rmtree(hp.datapath)
         create_data()
 
-    train_cnn()
-    #train_transformer()
-    #train_seq2seq()
+    if hp.train != "non_cnn":
+        train_cnn()
+
+    if hp.train == "seq2seq":
+        train_seq2seq()
+
+    elif hp.train == "transformer":
+        train_transformer()
+
+    elif hp.train == "non_cnn":
+        train_seq2seq_v2()
 
 
 # get test output
-if inference:
-    #test()
-    eval()
-    #eval_seq2seq()
-    #test()
+if hp.train == "infer":
+
+    if hp.infer == "cnn":
+        test()
+    elif hp.infer == "transformer":
+        eval_tf()
+    elif hp.infer == "seq2seq":
+        eval_seq2seq(mode="txt")
+    elif hp.infer == "non_cnn":
+        eval_seq2seq_v2()
